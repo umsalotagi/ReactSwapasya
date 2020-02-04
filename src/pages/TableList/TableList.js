92b91d70bc472712b7e60c78033868b56b920c31
Umesh <Umesh@DOLORES>
2020-02-05 00:37:41 +0530

    Added many things

--- src/pages/TableList/TableList.js
+++ src/pages/TableList/TableList.js
@@ -18,7 +18,7 @@ import {searchPersonByID, searchPersonByName} from './PersonService';
 
 // Issue 1 : if role property is not given, this causes an error
 
-import {Form_input} from "../shared_components/components1";
+import {Form_req_input, Form_input, Form_select, Form_datePicker} from "../shared_components/components1";
 
 
 const { Option } = Select;
@@ -37,6 +37,9 @@ class Demo extends React.Component {
 
   render() {
     const { getFieldDecorator } = this.props.form;
+    if (this.props.hidden) {
+      return <div> </div>
+    }
     return (
       <div>
       <Form layout="inline" onSubmit={this.handleSubmit}>
@@ -78,6 +81,10 @@ class PersonModule extends React.Component {
         super();
     this.state = {
       data: [],
+      hideSearchPerson: false,
+      hideViewPerson: true,
+      hideEditPerson: true,
+      personClicked: {}
     }
   }
 
@@ -93,16 +100,49 @@ class PersonModule extends React.Component {
         this.setState({data: [data1]})
       });
     }
+  };
+
+  viewPerson = (record) => {
+    console.log("Clicked view person");
+    console.log(record);
+    this.setState({hideSearchPerson: true, hideViewPerson: false, hideEditPerson: true, personClicked: record});
+  };
+
+  editPerson = (record) => {
+    console.log("edit person ...");
+    console.log(record);
+    this.setState({hideSearchPerson: true, hideViewPerson: true, hideEditPerson: false, personClicked: record});
+  };
+
+  deletePerson = (record) => {
+    this.setState({personClicked: record});
+  };
+
+  back = () => {
+    this.setState({hideSearchPerson: false, hideViewPerson: true, hideEditPerson: true});
   }
 
   render() {
     return (
       <div>
-      <PersonSearchForm handleSubmit={this.handleSubmit}/>
-      <PersonTable data={this.state.data}/>
+      <PersonSearchForm handleSubmit={this.handleSubmit} hidden={this.state.hideSearchPerson}/>
+      <PersonTable data={this.state.data} viewPerson={this.viewPerson}
+        editPerson={this.editPerson} deletePerson={this.deletePerson} hidden={this.state.hideSearchPerson}/>
+      <EditPersonForm personData={this.state.personClicked} hidden={this.state.hideEditPerson} back={this.back} />
+      <ViewPerson personData={this.state.personClicked} hidden={this.state.hideViewPerson} back={this.back}/>
       </div>
     );
   }
+}
+
+const View = ({text, record, viewPerson}) => {
+  const viewClick = e => {
+    viewPerson(record);
+  }
+
+  return (
+    <a onClick={viewClick} >View</a>
+  )
 }
 
 class PersonTable extends React.Component {
@@ -152,17 +192,20 @@ class PersonTable extends React.Component {
       key: 'action',
       render: (text, record) => (
         <span>
-          <a>View</a>
+          <View text={text} record={record} viewPerson={this.props.viewPerson} />
           <Divider type="vertical" />
-          <a>Edit</a>
+          <a onClick={() => {this.props.editPerson(record)}} >Edit</a>
           <Divider type="vertical" />
-          <a>Delete</a>
+          <a onClick={() => {this.props.deletePerson(record)}} >Delete</a>
         </span>
       ),
     },
   ];
 
   render() {
+    if (this.props.hidden) {
+      return <div> </div>
+    }
     return (
       <div>
       <Table rowKey="_id" columns={this.columns} dataSource={this.props.data} />
@@ -172,6 +215,63 @@ class PersonTable extends React.Component {
 }
 
 
+class EditPerson extends React.Component {
+
+  handleSubmit = () => {
+
+  }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const {personData, hidden, back} = this.props;
+    if (hidden) {
+      return <div> </div>
+    }
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    };
+    return (
+        <div>
+        <Button onClick={back} > Back </Button>
+        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+          <Divider orientation="left">Basic Details</Divider>
+          <Form_select getFieldDecorator={getFieldDecorator} label='Role' field='role' initial='Single' options={['Single', 'Married']} />
+          <Form_req_input getFieldDecorator={getFieldDecorator} label='Person ID' field='_id' msg='Please input id !'/>
+          <Form_req_input getFieldDecorator={getFieldDecorator} label='First Name' field='fistName' msg='Please input First Name !'/>
+          <Form_req_input getFieldDecorator={getFieldDecorator} label='Last Name' field='lastName' msg='Please input Last Name !'/>
+          <Form_input getFieldDecorator={getFieldDecorator} label='Middle Name' field='middleName' />
+          <Form_req_input getFieldDecorator={getFieldDecorator} label='Pnr No' field='_id' msg='Please input Pnr No!'/>
+          <Form_req_input getFieldDecorator={getFieldDecorator} label='Email' field='email' msg='Please input Email!'/>
+          <Form_input getFieldDecorator={getFieldDecorator} label='Mobile No' field='middleName' />
+          <Form_select getFieldDecorator={getFieldDecorator} label='Marital Status' field='middleName' initial='Single' options={['Single', 'Married']} />
+          <Form_datePicker getFieldDecorator={getFieldDecorator} label='Birth Date' field='bod' />
+
+          <Divider orientation="left">Other Details</Divider>
+
+          <Divider orientation="left">Address Details</Divider>
+        </Form>
+        </div>
+
+    );
+  }
+}
+
+const ViewPerson = ({personData, hidden, back}) => {
+  if (hidden) {
+    return <div> </div>
+  }
+  return (
+    <div> </div>
+  );
+}
+
+const EditPersonForm = Form.create({ name: 'customized_form_controls' })(EditPerson);
 
 export default function TableList() {
   return (
--- src/pages/shared_components/components1.js
+++ src/pages/shared_components/components1.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   Form,
+  DatePicker,
   Input,
   Tooltip,
   Icon,
@@ -13,20 +14,77 @@ import {
   AutoComplete,
 } from 'antd';
 
-export const Form_input = ({getFieldDecorator, label, req, msg}) => {
-
+export const Form_input1 = ({getFieldDecorator, label, req, msg}) => {
   return (
-  <Form.Item label={label}>
-    {getFieldDecorator(`${label}`, {
-      rules: [
-        {
-          required: `${req}`,
-          message: `${msg}`,
-        },
-      ],
-    })(<Input />)}
-  </Form.Item>
+  <Col span={12} >
+    <Form.Item label={label}>
+      {getFieldDecorator(`${label}`, {
+        rules: [
+          {
+            required: `${req}`,
+            message: `${msg}`,
+          },
+        ],
+      })(<Input />)}
+    </Form.Item>
+  </Col>
   );
 }
 
-//export {Form_input_req };
+export const Form_req_input = ({getFieldDecorator, label, field, msg}) => {
+  return (
+    <Col span={12} >
+      <Form.Item label={label}>
+        {getFieldDecorator(`${field}`, {
+          rules: [
+            {
+              required: true,
+              message: `${msg}`,
+            }
+          ],
+        }) (<Input />)}
+      </Form.Item>
+    </Col>
+  )
+}
+
+export const Form_input = ({getFieldDecorator, label, field}) => {
+  return (
+    <Col span={12} >
+      <Form.Item label={<span>{label}</span>}>
+        {getFieldDecorator(`${field}`, {
+
+        }) (<Input />)}
+      </Form.Item>
+    </Col>
+  )
+}
+
+const { Option } = Select;
+export const Form_select = ({getFieldDecorator, label, field, initial, options}) => {
+  return (
+    <Col span={12} >
+      <Form.Item label={label}>
+      {getFieldDecorator(`${field}`, { initialValue: `${initial}`})
+        (<Select size={300}>
+          {
+            options.map((op) => <Option value={op.toLowerCase()}>{op}</Option>)
+          }
+        </Select>)}
+      </Form.Item>
+    </Col>
+  )
+}
+
+const config = {
+      rules: [{ type: 'object'}],
+};
+export const Form_datePicker = ({getFieldDecorator, label, field}) => {
+  return (
+    <Col span={12} >
+      <Form.Item label={label}>
+        {getFieldDecorator(`${field}`, config)(<DatePicker />)}
+      </Form.Item>
+    </Col>
+  )
+}
